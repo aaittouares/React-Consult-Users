ce83ee6035448ba6199d6027da8663520e63f709
alexandre aittouares <alex.aittouares@gmail.com>
2019-05-06 20:47:45 +0200

    chap 30

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
+import UsersReducer from './reducer_users';
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  users: UsersReducer
+  
 });
 
 export default rootReducer;
